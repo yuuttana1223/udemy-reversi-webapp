3db96fadb7f8ee15f30fa08f98d765ce5c06ff64
yuuttana1223 <64090349+yuuttana1223@users.noreply.github.com>
2024-07-21 19:20:16 +0900

    ✨ 静的ファイルを追加

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ const PORT = 3000;
 const app = express();
 
 app.use(morgan("dev"));
+app.use(express.static("static", { extensions: ["html"] }));
 
 app.get("/api/hello", async (req, res) => {
   res.json({ message: "Hello World" });
