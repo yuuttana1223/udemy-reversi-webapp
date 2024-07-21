efa069bff8753c0461dc065320092954c877e48d
yuuttana1223 <64090349+yuuttana1223@users.noreply.github.com>
2024-07-21 17:43:38 +0900

    ➕ 🔊 エラーハンドリング

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import "express-async-errors";
 
 const PORT = 3000;
 const app = express();
@@ -10,6 +11,22 @@ app.get("/api/hello", async (req, res) => {
   res.json({ message: "Hello World" });
 });
 
+app.get("/api/error", async (req, res) => {
+  throw new Error("error endpoint");
+});
+
+app.use(errorHandler);
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+function errorHandler(
+  err: any,
+  _req: express.Request,
+  res: express.Response,
+  _next: express.NextFunction
+) {
+  console.error("Unexpected error occurred", err);
+  res.status(500).send({ message: "Unexpected error occurred" });
+}
