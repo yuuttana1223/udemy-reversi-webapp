6b7e84d7cb2f6d1092464ad4db050a568ae9abc5
yuuttana1223 <64090349+yuuttana1223@users.noreply.github.com>
2024-11-25 08:27:50 +0900

    ✨ 盤面におけるかチェックする処理を追加

--- src/domain/turn/board.ts
+++ src/domain/turn/board.ts
@@ -1,6 +1,7 @@
 import { INITIAL_BOARD } from "../../application/constants";
 import { Disc } from "./disc";
 import { Move } from "./move";
+import { Point } from "./point";
 
 export class Board {
   constructor(private _discs: Disc[][]) {}
@@ -11,6 +12,23 @@ export class Board {
 
   place(move: Move): Board {
     // TODO: 盤面に置けるかチェック
+    if (this._discs[move.point.y][move.point.x] !== Disc.Empty) {
+      throw new Error(
+        `Selected point is not empty. x: ${move.point.x}, y: ${move.point.y}`
+      );
+    }
+
+    // ひっくり返せる点を列挙
+    const flipPoints = this.listFlipPoints();
+
+    // ひっくり返せる点がない場合は置けない
+    if (flipPoints.length === 0) {
+      throw new Error(
+        `Cannot place disc at x: ${move.point.x}, y: ${move.point.y}`
+      );
+    }
+
+    //
     // 盤面をコピー
     const newDiscs = this._discs.map((row) => {
       return row.map((disc) => {
@@ -24,6 +42,10 @@ export class Board {
     // ひっくり返す
 
     return new Board(newDiscs);
+  }
+
+  private listFlipPoints(): Point[] {
+    return [new Point(0, 0)];
   }
 }
 
