e175d6396daaf0ca80fe8ab9eb68227db9216f13
yuuttana1223 <64090349+yuuttana1223@users.noreply.github.com>
2024-11-26 09:02:55 +0900

    ✨ 順番を表示するようにする

--- static/main.js
+++ static/main.js
@@ -3,12 +3,15 @@ const DARK = 1;
 const LIGHT = 2;
 
 const boardElement = document.getElementById("board");
+const nextDiscMessageElement = document.getElementById("next-disc-message");
 
 async function showBoard(turnCount) {
   const response = await fetch(`/api/games/latest/turns/${turnCount}`);
   const responseBody = await response.json();
   const board = responseBody.board;
   const nextDisc = responseBody.nextDisc;
+
+  showNextDiscMessage(nextDisc);
 
   while (boardElement.firstChild) {
     boardElement.removeChild(boardElement.firstChild);
@@ -30,7 +33,10 @@ async function showBoard(turnCount) {
       } else {
         squareElement.addEventListener("click", async () => {
           const nextTurnCount = turnCount + 1;
-          await registerTurn(nextTurnCount, nextDisc, x, y);
+          const res = await registerTurn(nextTurnCount, nextDisc, x, y);
+          if (!res.ok) {
+            return;
+          }
           await showBoard(nextTurnCount);
         });
       }
@@ -38,6 +44,15 @@ async function showBoard(turnCount) {
       boardElement.appendChild(squareElement);
     });
   });
+}
+
+function showNextDiscMessage(nextDisc) {
+  if (nextDisc) {
+    const color = nextDisc === DARK ? "黒" : "白";
+    nextDiscMessageElement.textContent = `次は${color}の番です`;
+  } else {
+    nextDiscMessageElement.textContent = "";
+  }
 }
 
 async function registerGame() {
@@ -56,13 +71,14 @@ async function registerTurn(turnCount, disc, x, y) {
     },
   };
 
-  await fetch("/api/games/latest/turns", {
+  const res = await fetch("/api/games/latest/turns", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(requestBody),
   });
+  return res;
 }
 
 async function main() {
